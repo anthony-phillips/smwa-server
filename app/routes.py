4324e039694a4aaa00295e46848e0e0ba167ce92
anthony-phillips <aphillips196@gmail.com>
2019-12-01 23:48:13 -0500

    Prediction, time ranges for consumption

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,14 @@
 from app import app
 import numpy as np
 from scipy.integrate import trapz
+from scipy.stats import linregress
 from flask import request
 from datetime import datetime
+import math
 
 readings = []
+
+date_format = '%d%m%y%H%M%S'
 
 @app.route('/')
 @app.route('/index')
@@ -19,14 +23,41 @@ def upload():
 
 @app.route('/read/now')
 def read_now():
+   return str(readings[-1])
+
+@app.route('/read/all')
+def read_all():
    return str(readings)
 
 @app.route('/analytics/consumption')
 def analytics_consumption():
-   consumption = trapz([reading[0] for reading in readings],
-                       [reading[1] for reading in readings])
-   return str(consumption / 3600) # 3600 WS in a WH
+   start = request.args.get('start')
+   end = request.args.get('end')
+   start_time = readings[0][1]
+   end_time = math.inf
+   if start:
+      start_time = datetime.strptime(start, date_format).timestamp()
+   if end:
+      end_time = datetime.strptime(end, date_format).timestamp()
+   interval = filter(lambda reading: start_time <= reading[1] <= end_time, readings)
+   interval = list(interval)
+   consumption = trapz([reading[0] for reading in interval],
+                       [reading[1] for reading in interval])
+   return str(consumption / 3600) # 3600 watt-seconds in a watt-hour
 
 @app.route('/analytics/prediction')
 def analytics_prediction():
-   return str(readings)
+   start = request.args.get('start')
+   end = request.args.get('end')
+   start_time = readings[0][1]
+   if start:
+      start_time = datetime.strptime(start, date_format).timestamp()
+   end_time = datetime.strptime(end, date_format).timestamp()
+   interval = filter(lambda reading: start_time <= reading[1] <= end_time, readings)
+   interval = list(interval)
+   m,b,r,p,e = linregress([reading[1] for reading in interval],
+                          [reading[0] for reading in interval])
+   def P(t):
+      return m * t + b
+   dx = end_time-start_time
+   return str((P(start_time)*dx - (P(start_time)-P(end_time))*dx / 2)/3600)
